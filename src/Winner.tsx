aaa0e8c73d7394d282763f1ec368e22a7131629f
nails8919 <shorty_love_12@hotmail.com>
2025-10-17 11:54:43 -0400

    Button Code Passed 2

--- src/Winner.tsx
+++ src/Winner.tsx
@@ -6,7 +6,7 @@ const Winner = (props) => {
     return (
         <div className="flex flex-col items-center -gap-4">
             <h1 className="font-bold text-4xl text-rose-700 flex animate-pulse">Winner, Winner, Chicken Dinner!</h1>
-            <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600" onCLick ={handleReset}>Restart Game</button>
+            <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600" onClick ={handleReset}>Restart Game</button>
         </div>
     )
 }
